8f18f847e168230e971a12e784306262f214d9d2
JohnnyLinares <j.linares@asesoriamadeeasy.com>
2026-01-21 23:43:58 -0500

    Fix4

--- lang/en/visorpdf.php
+++ lang/en/visorpdf.php
@@ -5,5 +5,5 @@ $string['modulenameplural'] = 'Visores PDF Drive';
 $string['pluginname'] = 'Visor PDF Drive';
 $string['visorpdf:addinstance'] = 'Add a new Visor PDF';
 $string['visorpdf:view'] = 'View Visor PDF';
-$string['pluginname'] = 'Visor PDF Drive';
 $string['file'] = 'File from Drive';
+$string['novisorpdfs'] = 'There are no Visor PDF activities in this course.';
---
+++ lang/es/visorpdf.php
@@ -0,0 +1,17 @@
+<?php
+// Cadenas en español para visorpdf.
+
+$string['pluginname']          = 'Visor PDF desde Drive';
+$string['modulename']          = 'PDF de Drive';
+$string['modulenameplural']    = 'PDFs de Drive';
+$string['modulename_help']     = 'Muestra un PDF almacenado en Google Drive usando el visor embebido de Drive. El profesor solo debe seleccionar el archivo.';
+
+$string['pluginadministration'] = 'Administración de PDF de Drive';
+
+$string['file']           = 'Archivo de Drive';
+$string['driveurl_help']  = 'Selecciona el archivo PDF de Google Drive que deseas mostrar.';
+
+$string['novisorpdfs']    = 'No hay actividades de PDF de Drive en este curso.';
+
+$string['visorpdf:addinstance'] = 'Añadir una nueva actividad PDF de Drive';
+$string['visorpdf:view']        = 'Ver actividad PDF de Drive';
